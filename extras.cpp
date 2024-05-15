1ee8d1d343d99d2a03518369542daadbefcc9138
Iniquez Rusk Jr <iniquezrusk@gmail.com>
2024-05-15 15:37:39 -0500

    Create extras.cpp

---
+++ extras.cpp
@@ -0,0 +1,21 @@
+Sides Menu:
+
+Waffle Potato Fries (M) - 2.19$
+Waffle Potato Fries (L) - 2.49$
+Waffle potato Chips - 2.49$
+
+Beverages menu:
+
+Normal Beverages:
+ S - 1.49$
+ M - 1.89$
+ L - 2.29$
+
+Sprite
+Pepsi
+HI-C
+Fruit Punch Gatorade
+Orange Fanta
+Dr Pepper
+Coca-Cola
+Water
